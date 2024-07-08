4730fa72896e6944ddf4a8f5164ae09346653d24
Mehrsa <mehrsaref@gmail.com>
2024-07-08 17:10:40 +0330

    Fix: search function merged in filter function

--- source/js/betterScript.js
+++ source/js/betterScript.js
@@ -1,170 +1,168 @@
-let toDoArray = []
+let taskArray = []
+let classifiedOutput = []
+let searchOutput = []
 let toDo =  {}
 let activeBtn = 'all'
+let searchQuery
 let $ = document
-let inputTask = $.querySelector('.form-control')
-let allTasks = $.querySelector('.all--tasks')
-let doneTasks = $.querySelector('.done--tasks')
-let undoneTasks = $.querySelector('.undone--tasks')
+let searchInput = $.querySelector('.search--input')
+let searchList = $.querySelector('.search--list')
+let toDoLabel = $.querySelector('.todo--label')
+let inputTask = $.querySelector('.todo--input')
+let taskList = $.querySelector('.task--list')
 let btnAll = $.querySelector('.all--btn')
 let btnDone = $.querySelector('.done--btn')
 let btnUndone = $.querySelector('.undone--btn')
-let topLabel = $.querySelector('label')
-let searchInput = $.querySelector('.search')
-let searchResult = $.querySelector('.search--result')
-
 searchInput.addEventListener('keydown',function (event){
     if(event.key === 'Enter'){
-        if(searchInput.value){
-            console.log(searchInput.value)
-            search(searchInput.value)
+        searchQuery = searchInput.value
+        if(searchQuery){
+            activeBtn = 'search'
+            filterArray()
             searchInput.value = ''
         }
     }
 })
-btnAll.addEventListener('focus', function (){
-    allTasks.style.display = 'block'
-    doneTasks.style.display = 'none'
-    undoneTasks.style.display = 'none'
-    topLabel.innerHTML = 'Tasks to do &#8595'
-    activeBtn = 'all'
-    searchResult.style.display = 'none'
-    btnAll.style.filter = 'brightness(.9)'
-})
-btnDone.addEventListener('focus', function (){
-        doneTasks.style.display = 'block'
-        allTasks.style.display = 'none'
-        undoneTasks.style.display = 'none'
-        topLabel.innerHTML = 'Completed today &#8595'
-        btnAll.style.filter = 'brightness(1)'
-        activeBtn = 'done'
-        searchResult.style.display = 'none'
-})
-btnUndone.addEventListener('focus', function (){
-    undoneTasks.style.display = 'block'
-    allTasks.style.display = 'none'
-    doneTasks.style.display = 'none'
-    topLabel.innerHTML = 'In progress &#8595'
-    btnAll.style.filter = 'brightness(1)'
-    activeBtn = 'undone'
-    searchResult.style.display = 'none'
-})
 inputTask.addEventListener('keydown',function (event) {
-    if (event.key === 'Enter') {
+    if(event.key === 'Enter'){
         event.preventDefault()
-        if (inputTask.value) {
+        if (inputTask.value){
+            taskArray = taskArray.filter(function (todo){
+                return todo.name !== inputTask.value
+            })
             toDo =  {name:'', checked:'false'}
             toDo.name = inputTask.value.trim()
-            toDoArray.push(toDo)
+            taskArray.push(toDo)
+            classifiedOutput.push(toDo)
             inputTask.value = ''
-            update()
+            filterArray()
         }
     }
+})
+btnAll.addEventListener('focus', function (){
+    toDoLabel.innerHTML = 'Tasks to do &#8595'
+    activeBtn = 'all'
+    btnAll.style.filter = 'brightness(.9)'
+    filterArray()
+})
+btnDone.addEventListener('focus', function (){
+    toDoLabel.innerHTML = 'Completed today &#8595'
+    activeBtn = 'done'
+
+    btnAll.style.filter = 'brightness(1)'
+    filterArray()
+})
+btnUndone.addEventListener('focus', function (){
+    toDoLabel.innerHTML = 'In progress &#8595'
+    activeBtn = 'undone'
+    btnAll.style.filter = 'brightness(1)'
+    filterArray()
 })
 
 function addTask(toDo){
     let newTask = $.createElement('li')
     newTask.className = 'list-group-item d-flex justify-content-between align-items-center'
-    let taskText = $.createElement('span')
-    taskText.innerHTML = toDo.name
     let taskCheckBox = $.createElement('input')
     taskCheckBox.className = 'checkbox--style'
     taskCheckBox.type ='checkbox'
+    let taskText = $.createElement('span')
+    taskText.innerHTML = toDo.name
     let deleteIcon = $.createElement('i')
     deleteIcon.className ='fa fa-trash-o delete'
-    if (toDo.checked === 'true') {
-        taskCheckBox.checked = true
-        newTask.style.backgroundColor = '#dce1fd'
-        newTask.style.filter = 'brightness(1.05)'
-        taskText.style.textDecoration = 'line-through'
-        taskText.style.color = 'grey'
-        deleteIcon.style.color = 'grey'
-    } else {
-        taskCheckBox.checked = false
-        newTask.style.backgroundColor = '#fff'
-        taskText.style.textDecoration = 'none'
-        taskText.style.color = 'black'
-        deleteIcon.style.color = 'black'
-    }
+    taskCheckBox.checked = toDo.checked === 'true';
+
     taskCheckBox.addEventListener('click', function (event){
+        let removedItem
         if(event.target.checked){
-            toDoArray.find(function (todo){
+            taskArray.find(function (todo){
                 if(todo.name === event.target.nextSibling.innerHTML){
                     todo.checked = 'true'
+                    removedItem = todo
                 }
             })
+            taskArray = taskArray.filter(function (todo){
+                return todo.name !== removedItem.name
+            })
+            taskArray.push(removedItem)
         } else {
-            toDoArray.find(function (todo){
+            taskArray.find(function (todo){
                 if(todo.name === event.target.nextSibling.innerHTML){
                     todo.checked = 'false'
                 }
             })
         }
-        update()
+        filterArray()
     })
     deleteIcon.addEventListener('click',function (event){
-        toDoArray = toDoArray.filter(function (todo){
+        taskArray = taskArray.filter(function (todo){
+            if(todo.name === event.target.previousSibling.innerHTML){
+                event.target.parentElement.remove()
+            }
             return todo.name !== event.target.previousSibling.innerHTML
         })
-        update()
+        filterArray()
     })
     newTask.append(taskCheckBox,taskText,deleteIcon)
     return newTask
 }
 
-function update(){
-    updateDone()
-    updateUndone()
-    updateAllTasks()
-}
-function updateAllTasks(){
-    allTasks.replaceChildren()
-    toDoArray.forEach(function (todo){
-        allTasks.appendChild(addTask(todo))
-    })
-}
-function updateDone(){
-    doneTasks.replaceChildren()
-    toDoArray.forEach(function (todo){
-        if(todo.checked === 'true'){
-            doneTasks.appendChild(addTask(todo))
-        }
-    })
-}
-function updateUndone(){
-    undoneTasks.replaceChildren()
-    toDoArray.forEach(function (todo){
-        if(todo.checked === 'false'){
-            undoneTasks.appendChild(addTask(todo))
-        }
-    })
-}
-
-function search(value){
-    searchResult.replaceChildren()
-    searchResult.style.display = 'block'
-    if(activeBtn === 'all'){
-        allTasks.style.display = 'none'
-        toDoArray.forEach(function (todo){
-            if(todo.name === value){
-                searchResult.appendChild(addTask(todo))
-            }
+function filterArray(){
+    searchList.style.display = 'none'
+    taskList.style.display = 'block'
+    taskList.replaceChildren()
+    if (activeBtn === 'all') {
+        classifiedOutput = []
+        classifiedOutput = taskArray
+        inputTask.placeholder = 'Add a new task'
+        classifiedOutput.forEach(function (todo) {
+            taskList.appendChild(addTask(todo))
         })
     }
     if(activeBtn === 'done'){
-        doneTasks.style.display = 'none'
-        toDoArray.forEach(function (todo){
-            if(todo.name === value && todo.checked === 'true'){
-                searchResult.appendChild(addTask(todo))
-            }
+        classifiedOutput = []
+        classifiedOutput = taskArray.filter(function (todo){
+            return todo.checked === 'true'
         })
+        if(!classifiedOutput.length){
+            inputTask.placeholder = 'Empty!'
+        } else{
+            classifiedOutput.forEach(function (todo){
+                taskList.appendChild(addTask(todo))
+                inputTask.placeholder = 'Add a new task'
+            })
+        }
     }
     if(activeBtn === 'undone'){
-        undoneTasks.style.display = 'none'
-        toDoArray.forEach(function (todo){
-            if(todo.name === value && todo.checked === 'false'){
-                searchResult.appendChild(addTask(todo))
+        classifiedOutput = []
+        classifiedOutput = taskArray.filter(function (todo){
+            return todo.checked === 'false'
+        })
+        if(!classifiedOutput.length){
+            inputTask.placeholder = 'Empty!'
+        } else{
+            classifiedOutput.forEach(function (todo){
+                taskList.appendChild(addTask(todo))
+                inputTask.placeholder = 'Add a new task'
+            })
+        }
+    }
+    if(activeBtn === 'search'){
+        searchOutput = []
+        classifiedOutput.find(function (todo){
+            if(todo.name.includes(searchQuery)){
+                searchOutput.push(todo)
             }
+        })
+        if(!searchOutput.length){
+            inputTask.placeholder = 'No result for "'+ searchQuery + '"'
+        } else {
+            inputTask.placeholder = 'search results for "'+ searchQuery + '":'
+        }
+        searchList.style.display = 'block'
+        taskList.style.display = 'none'
+        searchList.replaceChildren()
+        searchOutput.forEach(function (todo){
+            searchList.appendChild(addTask(todo))
         })
     }
 }
